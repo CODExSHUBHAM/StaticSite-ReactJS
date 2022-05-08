3d3572afdebc93a6446d863cca81be4b8fdb6f07
CODExSHUBHAM <Shubham.nh10@gmail.com>
2022-05-08 14:56:43 +0530

    It will delete all the CSS files & Move the APP Styling to Tailwind CSS

---
+++ Task 1/JS/tailwindconfig.js
@@ -0,0 +1,28 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primary: '#001f46',
+                secondary: '#ffa100',
+                link: 'rgb(170, 204, 247);',
+                footer: 'rgb(51, 51, 51)'
+            },
+            backgroundImage: {
+                'hero': "url('../Assets/bg.jpeg')"
+            },
+            fontFamily: {
+
+                'sans': ['Open Sans', 'sans-serif'],
+                'sans-m':['Montserrat, sans-serif'],
+                'serif': ['Merriweather', 'serif']
+
+            },
+            letterSpacing: {
+                button: '4px'
+            },
+            lineHeight:{
+                btn : '22.4px' 
+            }
+        }
+    }
+}
