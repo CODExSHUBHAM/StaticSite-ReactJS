a7fe28ef5e54d634396a1c49f261957f59a0d0e4
Shubham Kashyap <Shubham.nh10@gmail.com>
2022-05-23 22:54:33 +0530

    change in main js file

--- Task 1/JS/main.js
+++ Task 1/JS/main.js
@@ -1,5 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
+console.log('hello world')
+
 // Create a single supabase client for interacting with your database 
 const supabase = createClient('https://bvlrhkxalnoqevcsspoz.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ2bHJoa3hhbG5vcWV2Y3NzcG96Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTI5NDU3OTAsImV4cCI6MTk2ODUyMTc5MH0.spwhylI9YfNQZCzQNkRJWeKMtDvmBsfKGWScCExsu2c')
 
@@ -10,7 +12,7 @@ async function loadData() {
             .select()
 
     console.log(data)
-    console.log("1")
+    console.log("error")
 }
 
 loadData()
