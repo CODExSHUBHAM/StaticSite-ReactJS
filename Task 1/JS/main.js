62fd0d603163b962bc47b417d4ac3325f8281c77
Shubham Kashyap <Shubham.nh10@gmail.com>
2022-05-23 13:15:44 +0530

    add supabase using npm

--- Task 1/JS/main.js
+++ Task 1/JS/main.js
@@ -1,11 +1,16 @@
+import { createClient } from '@supabase/supabase-js'
 
-// async function loadData() {
-//     const { data, error } = await supabase
-//             .from('Features')
-//             .select()
+// Create a single supabase client for interacting with your database 
+const supabase = createClient('https://bvlrhkxalnoqevcsspoz.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ2bHJoa3hhbG5vcWV2Y3NzcG96Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTI5NDU3OTAsImV4cCI6MTk2ODUyMTc5MH0.spwhylI9YfNQZCzQNkRJWeKMtDvmBsfKGWScCExsu2c')
 
-//     console.log(data)
-//     console.log("1")
-// }
 
-// loadData()
+async function loadData() {
+    const { data, error } = await supabase
+            .from('Features')
+            .select()
+
+    console.log(data)
+    console.log("1")
+}
+
+loadData()
